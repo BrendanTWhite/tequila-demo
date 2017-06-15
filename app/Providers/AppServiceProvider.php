ee191bef54c61ba0c1f09c0e3415965946cf54da
Brendan T White <brendan@thespia.com>
2017-06-15 20:44:33 +0930

    We’ve now fixed the Driver [okta] not supported issue (thanks to https://github.com/mollie/laravel-mollie/issues/5 ) but are now getting NotFoundHttpException in RouteCollection.php line 161.  :-(

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Socialite;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Socialite::extend('okta', function ($app) {
+            $config = $app['config']['services.okta'];
+            return Socialite::buildProvider('Tequilarapido\Okta\OktaProvider', $config);
+        });
     }
 
     /**
